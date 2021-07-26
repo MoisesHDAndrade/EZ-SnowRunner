8b670eae4b3a0052cc48684cd8853ea3bb0e5a15
Moises Andrade <lawliex@gmail.com>
2021-07-26 13:07:30 +1200

    minor changes

--- app.py
+++ app.py
@@ -23,7 +23,6 @@ class App:
         self.sr_image = ImageTk.PhotoImage(self.sr_image)
         self.sr_image_label = ttk.Label(self.root, image=self.sr_image)
         self.sr_image_label.grid(row=0, column=0, columnspan=2, rowspan=2)
-        # self.label_image = PhotoImage(file=Image.open("./sr.jpeg"))
         self.label_money = ttk.Label(self.root, text="Money:")
         self.label_money.grid(row=2, column=0, sticky=E, padx=10, pady=5)
         self.money = StringVar()
@@ -56,12 +55,11 @@ class App:
         self.new_money = 0
         self.new_rank = 0
         self.new_experience = 0
-        # se existe um arquivo datfile ele le o endereco e mostra na entrada do arquivo
+
         if os.path.isfile('./datfile'):
             with open('datfile', 'r') as dat:
                 for item in dat.readlines():
                     self.file_path.insert(END, item)
-        # se nao existe um arquivo datfile ele cria um arquivo vazio para ser usado quando pressionar o botao de abrir arquivo
         else:
             with open('datfile', 'w') as dat:
                 dat.write('')
@@ -76,17 +74,16 @@ class App:
         self.root.destroy()
 
     def open_file(self):
-        # ao abrir deleta da entrada do arquivo para nao fazer append
+
         self.file_path.delete(0, END)
         self.filedialog = filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("dat files","*.dat"),("all files","*.*")))
         self.file_path.insert(END, self.filedialog)
        
-        # se abrir um arquivo ele salva o endereco no arquivo datfile
         if self.filedialog:
             with open('datfile', 'w') as dat:
                 dat.write(self.filedialog)
-                if os.path.basename(self.filedialog) == 'CompleteSave.dat': # checa se arquivo é CompleteSave.dat
-                    with open(self.filedialog, 'r') as file: #entao abre o arquivo CompleteSave.dat e define os valores para os campos do programa
+                if os.path.basename(self.filedialog) == 'CompleteSave.dat':
+                    with open(self.filedialog, 'r') as file:
                         dat_file = file.read()
                         money_read = re.search(r'"money":.\d+', dat_file).group(0)
                         self.money.set(re.search(r'\d+', money_read).group(0))
@@ -94,13 +91,12 @@ class App:
                         self.rank.set(re.search(r'\d+', rank_read).group(0))
                         experience_read = re.search(r'"experience":.\d+', dat_file).group(0)
                         self.experience.set(re.search(r'\d+', experience_read).group(0))
-                else: # se o arquivo aberto nao é CompleteSave.dat entao define os valores vazios para os campos do programa
+                else:
                     self.money.set('')
                     self.rank.set('')
                     self.experience.set('')
                     
-                    messagebox.showerror("Error",f"{os.path.basename(self.filedialog)} is not a expected file! \n \nCompleteSave.data usually it is on 'C:\\Users\[user]\\Documents\\My Games\\SnowRunner\\base\\storage\\[userid]'")
-        # se nao abrir um arquivo ele deixa salvo o ultimo endereco aberto no arquivo datfile
+                    messagebox.showerror("File not found",f"{os.path.basename(self.filedialog)} is not a expected file! \n \nCompleteSave.data usually it is on 'C:\\Users\[user]\\Documents\\My Games\\SnowRunner\\base\\storage\\[userid]'")
         else:
             with open('datfile', 'r') as dat:
                 for item in dat.readlines():
@@ -109,15 +105,13 @@ class App:
        
    
     def read_file_data(self):
-        if os.path.isfile('datfile'): # checa se ja existe um arquivo datfile
-            with open('datfile', 'r') as dat: # se existir abre o arquivo
+        if os.path.isfile('datfile'):
+            with open('datfile', 'r') as dat:
                 for item in dat.readlines():
-                    if os.path.isfile(item): # checa se o endereco corresponde a um arquivo existente
-                        with open(item, 'r') as file: # entao abre esse arquivo
-                            if os.path.basename(item) == 'CompleteSave.dat': # checa se arquivo é CompleteSave.dat
+                    if os.path.isfile(item):
+                        with open(item, 'r') as file:
+                            if os.path.basename(item) == 'CompleteSave.dat':
                                 self.new_file_dir = os.path.dirname(item)
-                                # self.write_json() # se for entao chama a funcao write_json para criar o arquivo CompleteSave.json
-                                # with open (item, 'r') as file:
                                 dat_file = file.read()
                                 money_read = re.search(r'"money":.\d+', dat_file).group(0)
                                 self.money.set(re.search(r'\d+', money_read).group(0))
@@ -143,10 +137,5 @@ class App:
                 messagebox.showinfo("Success", "Restart the game for changes make effect")
            
             
-          
-            
-                      
-
-
 if __name__ == "__main__":
    app = App()
